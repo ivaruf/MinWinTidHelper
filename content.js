61712acb6669fe74c8b241b5439dada60671103c
Ivar Fredriksen <ivar.fredriksen@byr.oslo.kommune.no>
2020-02-27 09:44:52 +0100

    Add logic to fill single ready day with no error handling

--- content.js
+++ content.js
@@ -1,7 +1,40 @@
 // content.js
 
+async function fillOut() {
+    document.getElementById("addInOut").click();
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    document.getElementById("reg-input-0").value = "09:00";
+    var inntid = $("#reg-input-0")
+
+    inntid.keydown();
+    inntid.keypress();
+    inntid.keyup();
+    inntid.blur();
+
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    document.getElementById("reg-input-86400").value = "16:35";
+    var uttid = $("#reg-input-86400");
+
+    uttid.keydown();
+    uttid.keypress();
+    uttid.keyup();
+    uttid.blur();
+
+    document.getElementById("calculate-button").click();
+}
+
+var script = document.createElement('script');
+script.textContent = fillOut.toString();
+(document.head||document.documentElement).appendChild(script);
+script.parentNode.removeChild(script);
+
 jQuery(document).ready(function() {
-    jQuery("#editing-day ul").append('<li> <button id="fyll-ut-dag" type="button"> Fyll ut dag </button></li>');
+    jQuery("#editing-day ul").append('<li> <button onClick="fillOut()" type="button"> Fyll ut dag </button></li>');
+    document.addEventListener('fillOutEvent', function() {
+        fillOut();
+    });
 });
-
-
