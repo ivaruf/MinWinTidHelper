c5657e32086782abff0ae336a729e171336d66ce
Ivar Fredriksen <ivar.fredriksen@byr.oslo.kommune.no>
2020-03-03 12:38:46 +0100

    Cleanup options

--- options.js
+++ options.js
@@ -10,10 +10,7 @@ function save_options() {
     }, function() {
       // Update status to let user know options were saved.
       var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
+      window.close();
     });
   }
 
@@ -30,5 +27,4 @@ function save_options() {
     });
   }
   document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',
-      save_options);
+  document.getElementById('save').addEventListener('click', save_options);
